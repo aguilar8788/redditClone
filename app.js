6e3f573283815717e7a0388cf3894c44907cd63e
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-07-22 01:32:18 -0600

    add filters

--- app.js
+++ app.js
@@ -12,15 +12,53 @@ var app = angular.module('redditClone', []);
 app.controller('indexController', [
   '$scope',
   function indexController($scope) {
-    $scope.message = 'Home page';
+     $scope.reverse = true;
     $scope.postForm = false;
-    $scope.postData = [];
-    $scope.formSubmit = (data) => {
+    $scope.commentFormView = false;
+    $scope.showComment = false;
+    $scope.showCommentToggle = function() {
+           $scope.showComment = $scope.showComment === false ? true : false;
+       };
+    $scope.postData = [{
+        'author': 'alex pickens james ya',
+        'title': "title",
+        'description': " description one ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+        'imageUrl': "https://mscedos.files.wordpress.com/2010/03/03-30-101.jpg",
+        'date': "Jul 21, 2016 12:15:07 AM",
+        'score': 0,
+        upScore: function(){
+          this.score +=1;
+        },
+        downScore : function(){
+          this.score -= 1;
+        },
+        commentCount: 1,
+        postComments: [{commentAuthor: "Pete", comment:"this is a comment"}]
+      }];
+
+    $scope.formSubmit = () => {
       $scope.postData.push({
           title: $scope.title,
           author: $scope.author,
           imageUrl: $scope.imageUrl,
-          description: $scope.description
+          description: $scope.postDescription,
+          score: 0,
+          upScore: function() {
+            this.score++;
+          },
+          downScore: function() {
+            this.score--;
+          },
+          date: new Date(),
+          commentCount: 1,
+          postComments: [{commentAuthor: "Pete", comment:"this is a comment"}],
+          addComment: function(data) {
+            this.postComments.push({
+              commentAuthor: data.commentAuthor.$viewValue,
+              comment: data.comment.$viewValue
+            })
+            this.commentCount++
+          }
       })
       $scope.postForm = false;
     }
